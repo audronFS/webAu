a33b2e748c83f4d45270795f16c5bf1b34221a97
Aurora <aurora.delrioperez@nandos.co.uk>
2022-01-01 16:37:09 +0100

    ultimately upload

---
+++ views/composition copy.js
@@ -0,0 +1,49 @@
+// external js: flickity.pkgd.js
+
+var utils = window.fizzyUIUtils;
+
+var carousel = document.querySelector(".carousel");
+var flkty = new Flickity(carousel, {
+  prevNextButtons: false,
+  pageDots: false,
+  wrapAround: true,
+  //   autoPlay: 3000,
+  lazyLoad: true,
+});
+// elements
+var cellsButtonGroup = document.querySelector(".button-group--cells");
+var cellsButtons = utils.makeArray(cellsButtonGroup.children);
+
+// cell select
+cellsButtonGroup.addEventListener("click", function (event) {
+  if (!matchesSelector(event.target, ".button")) {
+    return;
+  }
+  var value = 0;
+  var index = cellsButtons.indexOf(event.target);
+  switch (index) {
+    case 1:
+      value = 2;
+      break;
+    case 2:
+      value = 4;
+      break;
+    case 3:
+      value = 5;
+      break;
+    case 4:
+      value = 0;
+      break;
+  }
+  flkty.select(value);
+});
+// previous
+var previousButton = document.querySelector(".button--previous");
+previousButton.addEventListener("click", function () {
+  flkty.previous();
+});
+// next
+var nextButton = document.querySelector(".button--next");
+nextButton.addEventListener("click", function () {
+  flkty.next();
+});
--- views/composition.js
+++ views/composition.js
@@ -1,111 +1,49 @@
-// $(document).ready(function () {
-//     jQuery('[id=carousel]').each(function (ind, obj) {
-//         $(obj).click(function () {
+// // external js: flickity.pkgd.js
 
-//             $(obj).animate({
-//                 width: '500px',
-//                 height: "300px",
-//             });
+// var utils = window.fizzyUIUtils;
 
-//         });
-//         $("#aqui").click(function () {
-//             $(obj).animate({ width: '80px', height: "100px" });
-//         });
-
-//     });
+// var carousel = document.querySelector(".carousel");
+// var flkty = new Flickity(carousel, {
+//   prevNextButtons: false,
+//   pageDots: false,
+//   wrapAround: true,
+//   //   autoPlay: 3000,
+//   lazyLoad: true,
 // });
+// // elements
+// var cellsButtonGroup = document.querySelector(".button-group--cells");
+// var cellsButtons = utils.makeArray(cellsButtonGroup.children);
 
-// function allDisplay() {
-//   document.getElementsByClassName("carousel-cell").style.display = "block";
-// }
-
-// function elecDisplay() {
-//   document.getElementById("audiovisual").style.visibility = "none";
-// }
-// function audioDisplay() {
-//     document.getElementById("electronic").style.visibility = "none";
+// // cell select
+// cellsButtonGroup.addEventListener("click", function (event) {
+//   if (!matchesSelector(event.target, ".button")) {
+//     return;
 //   }
-
-$(document).ready(function () {
-  if ($(window).width() < 992) {
-    //   $("h1").css("font-size", "18px");
-    //   $("h2").css("font-size", "16px");
-    $("h3").css("font-size", "16px");
-    // $("h4").css("font-size", "8px");
-  }
-});
-
-$(document).ready(function () {
-  $(".audiovisual").css("display", "none");
-  $(".electronic").css("display", "none");
-  $(".instrumental").css("display", "none");
-  $(".orchestration").css("display", "none");
-  $(".carousel-cell").css("background", "url(../../images/whitepattern.jpg)");
-
-  $("#toggle").click(function () {
-    $(".container2").css("margin-bottom", "40%");
-  });
-
-  $("#All").click(function () {
-    $(".all").css("display", "block");
-    $(".audiovisual").css("display", "none");
-    $(".electronic").css("display", "none");
-    $(".instrumental").css("display", "none");
-    $(".orchestration").css("display", "none");
-    // $(".carousel-cell").css("background", "url(../../images/whitepattern.jpg)");
-    // $(".container2").css("margin-bottom", "25%");
-  });
-  $("#Electronic").click(function () {
-    $(".all").css("display", "none");
-    $(".audiovisual").css("display", "none");
-    $(".electronic").css("display", "block");
-    $(".instrumental").css("display", "none");
-    $(".orchestration").css("display", "none");
-    // $(".carousel-cell").css(
-    //   "backgroundColor",
-    //   "url(../../images/bluepattern-copy.png)"
-    // );
-  });
-  $("#Audiovisual").click(function () {
-    $(".all").css("display", "none");
-    $(".audiovisual").css("display", "block");
-    $(".electronic").css("display", "none");
-    $(".instrumental").css("display", "none");
-    $(".orchestration").css("display", "none");
-    // $(".carousel-cell").css("background", "url(../../images/whitepattern.jpg)");
-  });
-  $("#Instrumental").click(function () {
-    $(".all").css("display", "none");
-    $(".audiovisual").css("display", "none");
-    $(".electronic").css("display", "none");
-    $(".instrumental").css("display", "block");
-    $(".orchestration").css("display", "none");
-
-    // $(".carousel-cell").css("background", "url(../../images/whitepattern.jpg)");
-  });
-  $("#Orchestration").click(function () {
-    $(".all").css("display", "none");
-    $(".audiovisual").css("display", "none");
-    $(".electronic").css("display", "none");
-    $(".instrumental").css("display", "none");
-    $(".orchestration").css("display", "block");
-    // $(".carousel-cell").css("background", "url(../../images/whitepattern.jpg)");
-  });
-});
-
-// Crude way to check that the screen is at least 1024x768
-// if ($(window).width() <= 1024) {
-//     $(".composer").css("display", "none");
+//   var value = 0;
+//   var index = cellsButtons.indexOf(event.target);
+//   switch (index) {
+//     case 1:
+//       value = 2;
+//       break;
+//     case 2:
+//       value = 4;
+//       break;
+//     case 3:
+//       value = 5;
+//       break;
+//     case 4:
+//       value = 0;
+//       break;
 //   }
-
-// function myFunction() {
-//     var x = "Total Width: " + screen.width + "px";
-//     document.getElementById("demo").innerHTML = x;
-//   }
-
-//   if ($(window).width() < 960) {
-//     alert('Less than 960');
-//  }
-//  else {
-//     alert('More than 960');
-//  }
+//   flkty.select(value);
+// });
+// // previous
+// var previousButton = document.querySelector(".button--previous");
+// previousButton.addEventListener("click", function () {
+//   flkty.previous();
+// });
+// // next
+// var nextButton = document.querySelector(".button--next");
+// nextButton.addEventListener("click", function () {
+//   flkty.next();
+// });
